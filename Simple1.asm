0ba0fe9b90829dcaf4dbe4b511609e823bc1fbf9
LT1216 <LT1216@skbl-422-005.ph.ic.ac.uk>
2018-10-11 16:21:55 +0100

    20181011 loop delay implemented (probably in wrong branch)

--- Simple1.asm
+++ Simple1.asm
@@ -7,6 +7,9 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw	0x10		    ; determining delay start value
+	movwf	0x1F		    ; position start value of delay decrement
+	
 	movlw	0xFF		    
 	movwf	TRISD, ACCESS	    ; Port D to input
 	movff	PORTD, 0x05	    ; Getting port D value to storage
@@ -16,10 +19,22 @@ start
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	;movlw 	0x63		    
+	;movlw 	0x63
+	call	delay
 	movf	PORTD, W, ACCESS				    
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+	
+	;delay subroutine argument at 0x20
+
+delay	
+	movff 0x1F , 0x20	    ; reloading the initial value of decrement	
+delay_loop	decfsz 0x20	
+	bra delay_loop	    ; reloading the initial value of decrement
+	return
+	
+	
+	
 	
 	end
